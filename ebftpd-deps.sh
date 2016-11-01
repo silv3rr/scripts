5f62615432768d759d6bd10b681b730a8ea4a1ee
root <root@ip-172-31-23-107.eu-west-1.compute.internal>
2016-11-01 16:50:23 +0100

    missed a \

--- ebftpd-deps.sh
+++ ebftpd-deps.sh
@@ -3,7 +3,7 @@ for i in \
   libboost-thread-dev libboost-regex-dev libboost-iostreams-dev libboost-system-dev \
   libboost-filesystem-dev libboost-date-time-dev libboost-program-options-dev libboost-signals-dev \
   libssl-dev \
-  mongodb
+  mongodb \
   mongodb-dev
 do
     apt-get install $i -y
