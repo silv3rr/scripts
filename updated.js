3fbd4d921202ce431b638ead74c6879bf69a7e45
slv <silv3rr@users.noreply.github.com>
2017-12-13 20:42:04 +0100

    updated updated :)

--- updated.js
+++ updated.js
@@ -1,5 +1,20 @@
 function lastUpdated() {
-  var u = new Date(document.lastModified); var y = u.getFullYear(); var m = u.getMonth()+1; var d = u.getDate(); var H = u.getHours(); var M = u.getMinutes();
-  document.getElementById('lastUpdated').innerHTML = y + '-' + (m<10 ? '0' : '') + m + '-' + (d<10 ? '0' : '') + d + ' ' + (H<10 ? '0' : '') + H + ':' + (M<10 ? '0' : '') + M;
-}
+  var xhr = new XMLHttpRequest();
+  xhr.open("HEAD", "https://sscripts.ga/README.md", true);
+  xhr.onload = function (e) {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+	u = new Date(xhr.getResponseHeader("Last-Modified"));
+	var y = u.getFullYear(); var m = u.getMonth()+1; var d = u.getDate(); var H = u.getHours(); var M = u.getMinutes();
+	document.getElementById('lastUpdated').innerHTML = y + '-' + (m<10 ? '0' : '') + m + '-' + (d<10 ? '0' : '') + d + ' ' + (H<10 ? '0' : '') + H + ':' + (M<10 ? '0' : '') + M;
+      } else {
+	document.getElementById('lastUpdated').innerHTML = "1337-13-37 13:37";
+      }
+    }
+  };
+  xhr.onerror = function (e) {
+    console.error(xhr.statusText);
+  };
+  xhr.send(null);
+};
 window.onload = lastUpdated;
