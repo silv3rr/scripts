52c18d83482af22418771e6b581a82c3fa008bfe
slv <silv3rr@users.noreply.github.com>
2018-01-02 00:07:47 +0100

    small cosmetic changes

--- updated.js
+++ updated.js
@@ -6,9 +6,9 @@ function lastUpdated() {
       if (xhr.status === 200) {
 	u = new Date(xhr.getResponseHeader("Last-Modified"));
 	var y = u.getFullYear(); var m = u.getMonth()+1; var d = u.getDate(); var H = u.getHours(); var M = u.getMinutes();
-	document.getElementById('lastUpdated').innerHTML = y + '-' + (m<10 ? '0' : '') + m + '-' + (d<10 ? '0' : '') + d + ' ' + (H<10 ? '0' : '') + H + ':' + (M<10 ? '0' : '') + M;
+	document.getElementById('lastUpdated').innerHTML = "updated " + y + '-' + (m<10 ? '0' : '') + m + '-' + (d<10 ? '0' : '') + d + ' ' + (H<10 ? '0' : '') + H + ':' + (M<10 ? '0' : '') + M;
       } else {
-	document.getElementById('lastUpdated').innerHTML = "1337-13-37 13:37";
+	document.getElementById('lastUpdated').innerHTML = "updated n/a";
       }
     }
   };
