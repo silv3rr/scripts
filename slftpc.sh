98660c82b5988c1fccebdb43b51bb40d0ec11819
silver <silv3rr@users.noreply.github.com>
2020-02-08 18:31:34 +0100

    slftpc: cleanup & fixes

--- slftpc.sh
+++ slftpc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# slftp-cleaner 20161204 slv
+# slftp-cleaner 20200207 slv
 # keeps slftp dir nice and clean, most useful in cron
 
 SLDIR="/home/user/slFtp"
@@ -17,27 +17,37 @@ LOG_MAXSIZE="10240"		# log file may not exceed 10MB filesize
 LOG_MAXFILES="10"		# keep 10 log files
 BKP_DIR="backup"
 BKP_MAXFILES="5"		# keep 5 backup files
+BKP_MAXDEL="3"		        # dont delete more than 3 backup files
 DB_MAXSIZE="102400"		# db may not exceed 100MB filesize
 DB_KEEP="1 year ago"		# delete db records older than 1 year
+COMPRESS="xz"			# program to use for compression (e.g. bzip2)
 
 DATE=$( date +%Y-%m-%d )	# dont delete today/yesterdays files
+
+if [ ! -d "$SLDIR" ]; then
+  printf "slftp dir \"%s\" does not exist, exiting...\\n" "$SLDIR"
+  exit 1
+fi
+if [ ! -d "$SLDIR/logs" ]; then
+  mkdir "$SLDIR/logs"
+fi
 DATEAGO=$( date --date="1 day ago" +%Y-%m-%d )
 DISK_AVAIL=$( df -k $SLDIR | tail -1 | awk '{ print $4 }' )
 
 if [[ "$1" != "" && "$1" != "DEBUG" ]]; then
-  printf "slftp-cleaner\n\n"
-  printf "SLDIR=\"%s\"\nCLEAN_LOGS=%s CLEAN_BACKUPS=%s CLEAN_DB=%s\n\n" "$SLDIR" "$CLEAN_LOGS" "$CLEAN_BACKUPS" "$CLEAN_DB"
-  printf "run %s without arguments to start\n\n" "$(basename $0)"
+  printf "slftp-cleaner\\n\\n"
+  printf "SLDIR=\"%s\"\\nCLEAN_LOGS=%s CLEAN_BACKUPS=%s CLEAN_DB=%s\\n\\n" "$SLDIR" "$CLEAN_LOGS" "$CLEAN_BACKUPS" "$CLEAN_DB"
+  printf "run %s without arguments to start\\n\\n" "$(basename "$0")"
 fi
 if [ "$CLEAN_LOGS" = "1" ]; then
 LOG_DATE=$( date +%Y%m%d%H%M )
-  for i in $SLDIR/{slftp,precatcher}.log; do
+  for i in "$SLDIR"/{slftp,precatcher}.log; do
      if (( "$( du -k $i | awk '{ print $1 }' )" > "$LOG_MAXSIZE" )); then
-       cp $i ${i}_${LOG_DATE} && truncate -c -s 0 $i
+       cp $i "${i}_${LOG_DATE}" && truncate -c -s 0 $i
      fi
   done
-  LOG_NRFILES="$( ls -1 $SLDIR | grep -E slftp.log_[0-9]{12} | wc -l )"
-  LOG_OLDEST=$( ls -1t $SLDIR | grep -E slftp.log_[0-9]{12} | tail -1 )
+  LOG_NRFILES="$( ls -1 "$SLDIR/*.log" | grep -E slftp.log_[0-9]{12} | wc -l )"
+  LOG_OLDEST=$( ls -1t "$SLDIR/*.log" | grep -E slftp.log_[0-9]{12} | tail -1 )
   COUNT=0
   while [ "$LOG_NRFILES" -gt "$LOG_MAXFILES" ]; do
     # make sure we never delete more than 3 files
@@ -45,47 +55,47 @@ LOG_DATE=$( date +%Y%m%d%H%M )
       break 
     fi
     if [ "$1" = "DEBUG" ]; then echo "DEBUG: COUNT: $COUNT LOG_NRFILES: $LOG_NRFILES > LOG_MAXFILES: $LOG_MAXFILES LOG_OLDEST: $LOG_OLDEST"; fi
-    if [[ ! $LOG_OLDEST =~ ^$SLDIR/$BKP_DIR/slftp.log_($DATE|$DATEAGO) ]]; then
-      rm $SLDIR/$LOG_OLDEST
+    if [[ ! $LOG_OLDEST =~ ^$SLDIR/logs/slftp.log_($DATE|$DATEAGO) ]]; then
+      rm "$SLDIR/$LOG_OLDEST"
     else
       break
     fi
-    LOG_NRFILES="$( ls -1 $SLDIR | grep -E slftp.log_[0-9]{12} | wc -l )"
-    LOG_OLDEST=$( ls -1t $SLDIR | grep -E slftp.log_[0-9]{12} | tail -1 )
-    COUNT=$(( $COUNT + 1 ))
+    LOG_NRFILES="$( ls -1 "$SLDIR/logs" | grep -E slftp.log_[0-9]{12} | wc -l )"
+    LOG_OLDEST=$( ls -1t "$SLDIR/logs" | grep -E slftp.log_[0-9]{12} | tail -1 )
+    COUNT=$(( COUNT + 1 ))
   done
-  for i in $SLDIR/slftp.log_*[0-9][0-9][0-9][0-9]; do
-    if [ -s $i ]; then
+  for i in "$SLDIR"/logs/slftp.log_*[0-9][0-9][0-9][0-9]; do
+    if [ -s "$i" ]; then
       if (( "$DISK_AVAIL" > "$DISK_NEED" )); then
-         bzip2 $i
+        $COMPRESS "$i" && mv "$i"* "$SLDIR/logs"
       fi
     fi
   done
 fi
 
 if [ "$CLEAN_BACKUPS" = "1" ]; then
-  BKP_NRFILES="$( ls -1 $SLDIR/$BKP_DIR | grep -E slftp-backup-[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{6}.tar | wc -l )"
-  BKP_OLDEST=$( ls -1t $SLDIR/$BKP_DIR | grep -E slftp-backup-[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{6}.tar | tail -1 )
+  BKP_NRFILES="$( ls -1 "$SLDIR/$BKP_DIR" | grep -E slftp-backup-[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{6}.tar | wc -l )"
+  BKP_OLDEST=$( ls -1t "$SLDIR/$BKP_DIR" | grep -E slftp-backup-[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{6}.tar | tail -1 )
   COUNT=0
   while [ "$BKP_NRFILES" -gt "$BKP_MAXFILES" ]; do
-    # make sure we never delete more than 3 files
-    if (( "$COUNT" > "3" )); then
+    # make sure we never delete more than x files
+    if (( "$COUNT" > "$BKP_MAXDEL" )); then
       break 
     fi
     if [ "$1" = "DEBUG" ]; then echo "DEBUG: COUNT: $COUNT BKP_NRFILES: $BKP_NRFILES > BKP_MAXFILES: $BKP_MAXFILES BKP_OLDEST: $BKP_OLDEST"; fi
     if [[ ! $BKP_OLDEST =~ ^$SLDIR/$BKP_DIR/slftp-backup-($DATE|$DATEAGO) ]]; then
-      rm $SLDIR/$BKP_DIR/$BKP_OLDEST
+      rm "$SLDIR/$BKP_DIR/$BKP_OLDEST"
     else
       break
     fi
-    BKP_NRFILES="$( ls -1 $SLDIR/$BKP_DIR | grep -E slftp-backup-[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{6}.tar | wc -l )"
-    BKP_OLDEST=$( ls -1t $SLDIR/$BKP_DIR | grep -E slftp-backup-[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{6}.tar | tail -1 )
-    COUNT=$(( $COUNT + 1 ))
+    BKP_NRFILES="$( ls -1 "$SLDIR/$BKP_DIR" | grep -E slftp-backup-[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{6}.tar | wc -l )"
+    BKP_OLDEST=$( ls -1t "$SLDIR/$BKP_DIR" | grep -E slftp-backup-[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{6}.tar | tail -1 )
+    COUNT=$(( COUNT + 1 ))
   done 
-  for i in $SLDIR/$BKP_DIR/slftp-backup-[0-9][0-9][0-9][0-9]-*.tar; do
-    if [ -f $i ]; then
+  for i in "$SLDIR/$BKP_DIR"/slftp-backup-[0-9][0-9][0-9][0-9]-*.tar; do
+    if [ -f "$i" ]; then
       if (( "$DISK_AVAIL" > "$DISK_NEED" )); then
-         bzip2 $i
+        $COMPRESS "$i"
       fi
     fi
   done
@@ -96,7 +106,7 @@ DB_DATE=$( date --date="$DB_KEEP" +%Y-%m-%d )
   if [ -f "$SLDIR/stats.db" ]; then
     if (( "$( du -k $SLDIR/stats.db | awk '{ print $1 }' )" > "$DB_MAXSIZE" )); then
       sqlite3 $SLDIR/stats.db "DELETE FROM hit where ts < '$DB_DATE'"
-      sqlite3 $SLDIR/stats.db "VACUUM hit"
+      sqlite3 $SLDIR/stats.db "VACUUM"
     fi
   fi
 fi
