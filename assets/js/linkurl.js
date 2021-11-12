61afa251fd4ffc781c85c657c56f3df3036b5e44
silver <silv3rr@users.noreply.github.com>
2021-11-12 17:01:59 +0100

    links

--- assets/js/linkurl.js
+++ assets/js/linkurl.js
@@ -1,4 +1,4 @@
 function linkURL(text) {
-  var re = RegExp("(\\b((https?|ftp|file):\/\/|www)[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|]*)", "gi");
+  var re = RegExp("(\\b((https?|ftp|file|irc):\/\/|www)[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|]*)", "gi");
   document.documentElement.innerHTML = text.replace(re,"<a href='$1'>$1<\/a>");
 }
